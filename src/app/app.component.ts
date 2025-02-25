d4268ff25922803c7c7a3bfc3420433809e957da
Alex Rodriguez <alexcarvajal2404@gmail.com>
2025-02-25 13:37:20 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PinPad, PinPadClient} from "@placetopay/pinpad-sdk";
+
+const URL = 'https://pinpad-dev.placetopay.ws'
+// const URL = 'https://pinpad-UAT.placetopay.ws'
+const TOKEN = '189|KB38AU9OTgMORAIcrihdy5HP2yTduHsfVTxHh1Em'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +13,65 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pinpad-angular-test';
+
+  transactionId = '';
+  pinpad = new PinPad({
+    mode: 'static'
+  });
+  client = new PinPadClient(URL, TOKEN)
+
+
+  interval: any;
+
+  async ngOnInit(): Promise<void> {
+    await this.createTransaction();
+  }
+
+
+
+  async createTransaction() {
+    try {
+      const response = await this.client.createTransaction();
+      const positions = response.data.data.positions.split('').join(',')
+      await this.pinpad.render('#pinpad-render', positions);
+      this.transactionId = response.data.data.transactionId;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement
+    const formData = new FormData(form);
+    const pin = formData.get('pin') as string;
+
+    const format = 0;
+    const data = {
+      transactionId: this.transactionId,
+      format,
+      pin,
+      pan: 123481203912,
+    }
+
+
+    this.client.generatePinBlock(data)
+      .then(res => {
+        console.log(res)
+        // this.flashPaymentService.controller.pinBlock = res.data.data.pinblock;
+        // this.flashPaymentService.stepUpdate(2)
+      }).catch(err => {
+      console.log('catch', err)
+    })
+
+    if (pin) {
+      console.log('PIN Submitted:', pin);
+    } else {
+      console.error('No PIN received');
+    }
+  }
+
 }
